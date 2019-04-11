4e3072d88cb19c655a7d883a177b0fd84678933a
kglibrarian <karen.gutzman@northwestern.edu>
2019-04-10 21:29:34 -0500

    added some more code

--- sakila_sql_script.sql
+++ sakila_sql_script.sql
@@ -1,11 +1,23 @@
 -- Sakila SQL Script
+
+-- Make it so all of the following code will affect favorite_db --
+USE sakila;
+
 -- 1a. Display the first and last names of all actors from the table `actor`.
+SELECT first_name, last_name FROM sakila.actor;
 
 --  1b. Display the first and last name of each actor in a single column in upper case letters. Name the column `Actor Name`.
+SELECT concat(first_name, ' ', last_name) AS Actor_Name FROM sakila.actor;
 
--- 2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." What is one query would you use to obtain this information?
+-- 2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." 
+-- What is one query would you use to obtain this information?
+SELECT actor_id, first_name, last_name 
+FROM sakila.actor
+WHERE first_name = "Joe";
 
 -- 2b. Find all actors whose last name contain the letters `GEN`:
+-- SELECT * FROM sakila.actor WHERE last_name CONTAINS '"GEN"'
+-- SELECT * FROM MyTable WHERE Column1 CONTAINS '"Phrase one" And word2 And "Phrase Two"'
 
 -- 2c. Find all actors whose last names contain the letters `LI`. This time, order the rows by last name and first name, in that order:
 
