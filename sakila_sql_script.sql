1ca1c1035a0a4ff8c61af3913e8dd04ef2fa1217
kglibrarian <karen.gutzman@northwestern.edu>
2019-04-11 20:49:41 -0500

    up to 7

--- sakila_sql_script.sql
+++ sakila_sql_script.sql
@@ -1,6 +1,6 @@
 -- Sakila SQL Script
 
--- Make it so all of the following code will affect favorite_db --
+-- Make it so all of the following code will affect sakila_db --
 USE sakila;
 
 -- 1a. Display the first and last names of all actors from the table `actor`.
@@ -16,42 +16,91 @@ FROM sakila.actor
 WHERE first_name = "Joe";
 
 -- 2b. Find all actors whose last name contain the letters `GEN`:
--- SELECT * FROM sakila.actor WHERE last_name CONTAINS '"GEN"'
--- SELECT * FROM MyTable WHERE Column1 CONTAINS '"Phrase one" And word2 And "Phrase Two"'
+SELECT * FROM sakila.actor WHERE last_name LIKE '%GEN%';
 
--- 2c. Find all actors whose last names contain the letters `LI`. This time, order the rows by last name and first name, in that order:
+-- 2c. Find all actors whose last names contain the letters `LI`. This time, order the rows by last name and first name, 
+-- in that order:
+SELECT * FROM sakila.actor WHERE last_name LIKE '%LI%' ORDER BY 3, 2;
 
--- 2d. Using `IN`, display the `country_id` and `country` columns of the following countries: Afghanistan, Bangladesh, and China:
+-- 2d. Using `IN`, display the `country_id` and `country` columns of the following countries: Afghanistan,  
+-- Bangladesh, and China:
+SELECT country_id, country FROM sakila.country WHERE country IN ('Afghanistan', 'Bangladesh', 'China');
 
--- 3a. You want to keep a description of each actor. You don't think you will be performing queries on a description, so create a column in the table `actor` named `description` and use the data type `BLOB` (Make sure to research the type `BLOB`, as the difference between it and `VARCHAR` are significant).
+-- 3a. You want to keep a description of each actor. You don't think you will be performing queries on a description, 
+-- so create a column in the table `actor` named `description` and use the data type `BLOB` (Make sure to research 
+-- the type `BLOB`, as the difference between it and `VARCHAR` are significant).
+ALTER TABLE sakila.actor
+ADD description BLOB;
+SELECT * from sakila.actor;
+
 
 -- 3b. Very quickly you realize that entering descriptions for each actor is too much effort. Delete the `description` column.
 
+ALTER TABLE sakila.actor
+DROP COLUMN description; 
+SELECT * from sakila.actor;
+
 -- 4a. List the last names of actors, as well as how many actors have that last name.
+SELECT last_name, COUNT(last_name) FROM sakila.actor GROUP BY last_name;
 
--- 4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
+-- 4b. List last names of actors and the number of actors who have that last name, but only for names that 
+-- are shared by at least two actors
+SELECT last_name, COUNT(last_name) FROM sakila.actor GROUP BY last_name HAVING COUNT(last_name) >= 2;
 
--- 4c. The actor `HARPO WILLIAMS` was accidentally entered in the `actor` table as `GROUCHO WILLIAMS`. Write a query to fix the record.
+-- 4c. The actor `HARPO WILLIAMS` was accidentally entered in the `actor` table as `GROUCHO WILLIAMS`. 
+-- Write a query to fix the record.
+UPDATE sakila.actor
+SET first_name = 'HARPO'
+WHERE first_name = 'GROUCHO' and last_name = 'WILLIAMS';
+SELECT * FROM sakila.actor WHERE last_name = 'WILLIAMS';
 
--- 4d. Perhaps we were too hasty in changing `GROUCHO` to `HARPO`. It turns out that `GROUCHO` was the correct name after all! In a single query, if the first name of the actor is currently `HARPO`, change it to `GROUCHO`.
+-- 4d. Perhaps we were too hasty in changing `GROUCHO` to `HARPO`. It turns out that `GROUCHO` was the correct 
+-- name after all! In a single query, if the first name of the actor is currently `HARPO`, change it to `GROUCHO`.
+UPDATE sakila.actor
+SET first_name = 'GROUCHO'
+WHERE first_name = 'HARPO' and last_name = 'WILLIAMS';
+SELECT * FROM sakila.actor WHERE last_name = 'WILLIAMS';
 
 -- 5a. You cannot locate the schema of the `address` table. Which query would you use to re-create it?
+ -- Hint: [https://dev.mysql.com/doc/refman/5.7/en/show-create-table.html]
+ -- (https://dev.mysql.com/doc/refman/5.7/en/show-create-table.html)
+ SHOW CREATE TABLE sakila.address;
 
- -- Hint: [https://dev.mysql.com/doc/refman/5.7/en/show-create-table.html](https://dev.mysql.com/doc/refman/5.7/en/show-create-table.html)
-
--- 6a. Use `JOIN` to display the first and last names, as well as the address, of each staff member. Use the tables `staff` and `address`:
+-- 6a. Use `JOIN` to display the first and last names, as well as the address, of each staff member. 
+-- Use the tables `staff` and `address`:
+SELECT a.first_name, a.last_name, b.address 
+FROM sakila.staff a LEFT JOIN sakila.address b 
+ON a.address_id = b.address_id;
 
 -- 6b. Use `JOIN` to display the total amount rung up by each staff member in August of 2005. Use tables `staff` and `payment`.
+SELECT a.first_name, a.last_name, b.total_amount 
+FROM sakila.staff a LEFT JOIN 
+	(SELECT staff_id, SUM(amount) AS total_amount FROM sakila.payment GROUP BY 1) b 
+ON a.staff_id = b.staff_id;
 
--- 6c. List each film and the number of actors who are listed for that film. Use tables `film_actor` and `film`. Use inner join.
+-- 6c. List each film and the number of actors who are listed for that film. Use tables `film_actor` 
+-- and `film`. Use inner join.
+SELECT a.title, b.total_actors
+FROM sakila.film a INNER JOIN (SELECT film_id, COUNT(actor_id) AS total_actors FROM sakila.film_actor GROUP BY 1) b
+ON a.film_id = b.film_id;
 
 -- 6d. How many copies of the film `Hunchback Impossible` exist in the inventory system?
+SELECT COUNT(film_id) FROM sakila.inventory WHERE film_id IN (
+SELECT film_id FROM sakila.film
+WHERE title LIKE '%Hunchback Impossible%');
 
--- 6e. Using the tables `payment` and `customer` and the `JOIN` command, list the total paid by each customer. List the customers alphabetically by last name:
-
+-- 6e. Using the tables `payment` and `customer` and the `JOIN` command, list the total paid by each customer. 
+-- List the customers alphabetically by last name:
   -- ![Total amount paid](Images/total_payment.png)
+SELECT a.first_name, a.last_name, b.total_amount FROM sakila.customer a LEFT JOIN 
+	(SELECT customer_id, SUM(amount) AS total_amount FROM sakila.payment GROUP BY customer_id) b
+ON a.customer_id = b.customer_id
+ORDER BY a.last_name;
+    
+--  7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, 
+-- films starting with the letters `K` and `Q` have also soared in popularity. Use subqueries to display the titles 
+-- of movies starting with the letters `K` and `Q` whose language is English.
 
---  7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters `K` and `Q` have also soared in popularity. Use subqueries to display the titles of movies starting with the letters `K` and `Q` whose language is English.
 
 -- 7b. Use subqueries to display all actors who appear in the film `Alone Trip`.
 
